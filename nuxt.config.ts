dff420eeba8c77153bed595334c824cccab8292a
Guillaume Duhan <31253241+guillaumeduhan@users.noreply.github.com>
2022-12-26 15:19:59 +0000

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,13 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  alias: {
+    "@": "/<rootDir>",
+  },
+  css: ["~/assets/main.scss"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+});
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <div>hello with tailwindcss :)</div>
+</template>
+
+<style lang='scss'></style>
