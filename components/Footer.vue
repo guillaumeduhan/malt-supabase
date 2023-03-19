0fae2e73c7168b75586ea7fbfbcb4cb8f11b0675
Guillaume Duhan <31253241+guillaumeduhan@users.noreply.github.com>
2023-03-19 11:46:27 +0000

    commit

--- components/Footer.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <footer class="flex items-center justify-center py-12 text-slate-600">
-    <p class="text-sm font-normal text-black-500">Powered by <span class="font-bold text-sky-400">Printsy</span> —
-      print-on-demand manager.</p>
-  </footer>
-</template>
-
-<style lang='scss'></style>
--- components/Header.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <header class="border-b">
-    <img src="logo.png" alt="paper" class="h-32 mx-auto" />
-  </header>
-</template>
